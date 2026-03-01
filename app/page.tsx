9bbacb42604f46b3c5bc8048ae64687bcd554b2e
ChandaniSahu <rrrsahu2005@gmail.com>
2026-03-01 22:52:28 +0530

    build errors resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import MapView from "@/components/MapView"
+import dynamic from "next/dynamic"
+
+const MapView = dynamic(() => import("@/components/MapView"), {
+  ssr: false,
+})
 
 export default function Home() {
 
